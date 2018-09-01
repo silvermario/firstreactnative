8452b6a94e0fb6acf1ac818fdbd10a40f95e05bf
Mariusz Kopec <mariusz.kopec.ext@adcubum.com>
2018-09-01 12:25:37 +0200

    [#1.3] css

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
 
-export default class App extends React.Component {
+class Note extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={styles.item}>
+                    <Text style={styles.title}>{this.props.title}</Text>
+                    <Text style={styles.content}>{this.props.content}</Text>
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default class App extends Component {
 
     state = {
         items: new Array(20).fill(0).map((a, i) => i).map(i => ({
@@ -15,10 +28,7 @@ export default class App extends React.Component {
             <View style={styles.container}>
                 <ScrollView>
                     {this.state.items.map(({title, content}) => (
-                        <Text key={title} style={styles.paragraph}>
-                            <Text style={styles.title}>{title}</Text>
-                            <Text>{content}</Text>
-                        </Text>
+                        <Note style={styles.item} key={title} title={title} content={content}></Note>
                     ))}
                 </ScrollView>
             </View>
@@ -31,18 +41,23 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         justifyContent: 'center',
-    },
-    paragraph: {
-        margin: 24,
-        fontSize: 18,
-        textAlign: 'left',
-        color: '#34495e',
+        paddingTop: 20,
     },
     item: {
-        paddingHorizontal: 10,
+        margin: 24,
+        textAlign: 'left',
+        color: '#34495e',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
     },
     title: {
         fontWeight: 'bold',
-        marginVertical: 5,
+        marginVertical: 50,
+        height: 50,
+    },
+    content: {
+        height: 200,
+        marginVertical: 50,
     },
 });
